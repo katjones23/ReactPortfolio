77a068648918027f81fa3a5be7116fe6762076fe
Katherine Jones <kejone1414@gmail.com>
2020-07-27 18:25:05 -0400

    cleaning gh-pages

---
+++ precache-manifest.8b8ba18e84e34844795dd23e876b3f13.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b3169f2838cac75fdb0d072026f62c",
+    "url": "/ReactPortfolio/index.html"
+  },
+  {
+    "revision": "10cc4a9f7c99b00fccb6",
+    "url": "/ReactPortfolio/static/css/main.58109d7c.chunk.css"
+  },
+  {
+    "revision": "a8f6f903e80012edeb4b",
+    "url": "/ReactPortfolio/static/js/2.1116748b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactPortfolio/static/js/2.1116748b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10cc4a9f7c99b00fccb6",
+    "url": "/ReactPortfolio/static/js/main.b0fe0d1a.chunk.js"
+  },
+  {
+    "revision": "97a1c7d0b592075495b7",
+    "url": "/ReactPortfolio/static/js/runtime-main.ad87311a.js"
+  },
+  {
+    "revision": "c89c921648a0b48d184a7b89e8b276c4",
+    "url": "/ReactPortfolio/static/media/KatherineJonesResume.c89c9216.pdf"
+  },
+  {
+    "revision": "78548d9bea0de782732cd0bf5aaf0fc1",
+    "url": "/ReactPortfolio/static/media/backgroundImgTatiana.78548d9b.png"
+  },
+  {
+    "revision": "404ce0e2b7a16a13683b997a1b6d46e6",
+    "url": "/ReactPortfolio/static/media/budgettracker.404ce0e2.png"
+  },
+  {
+    "revision": "8b4c4b3115ede500b64a5861323403ed",
+    "url": "/ReactPortfolio/static/media/dayplanner.8b4c4b31.png"
+  },
+  {
+    "revision": "9a9d8b766060543ee4bd77c039f8f49e",
+    "url": "/ReactPortfolio/static/media/employeetemplate.9a9d8b76.gif"
+  },
+  {
+    "revision": "05416698963cd564c330bab04fdc5f34",
+    "url": "/ReactPortfolio/static/media/employeetracker.05416698.gif"
+  },
+  {
+    "revision": "507c7e9b117c8ddf0b300a71c97f5b3b",
+    "url": "/ReactPortfolio/static/media/n2movies.507c7e9b.png"
+  },
+  {
+    "revision": "86968766429b23480ba8861d6899b4e1",
+    "url": "/ReactPortfolio/static/media/notetaker.86968766.png"
+  },
+  {
+    "revision": "ebfd990b8e19429db8b58be01b93381d",
+    "url": "/ReactPortfolio/static/media/passwordgen.ebfd990b.png"
+  },
+  {
+    "revision": "12bf670a57a333f5225c69f16d9006e3",
+    "url": "/ReactPortfolio/static/media/profile.12bf670a.jpg"
+  },
+  {
+    "revision": "7c6fb1c52bbde244a80f7d8e7e958b5a",
+    "url": "/ReactPortfolio/static/media/quiz.7c6fb1c5.png"
+  },
+  {
+    "revision": "c2760d3b88e07d9d73b4032a18adfca9",
+    "url": "/ReactPortfolio/static/media/readme.c2760d3b.gif"
+  },
+  {
+    "revision": "087f1df3e588bb7ef9059ff526927fb7",
+    "url": "/ReactPortfolio/static/media/recipexchange.087f1df3.jpg"
+  },
+  {
+    "revision": "2eda5d4aadf000dca7e7b0d8afe67325",
+    "url": "/ReactPortfolio/static/media/rtsite.2eda5d4a.png"
+  },
+  {
+    "revision": "5d1c7779be28923456c4a89359b7ffaa",
+    "url": "/ReactPortfolio/static/media/userdirectory.5d1c7779.png"
+  },
+  {
+    "revision": "ef2df3add77b5fed02f33517426d9830",
+    "url": "/ReactPortfolio/static/media/weatherapp.ef2df3ad.png"
+  },
+  {
+    "revision": "4b4861c0e904d0a51f5775dbbcdcb716",
+    "url": "/ReactPortfolio/static/media/workouttracker.4b4861c0.png"
+  }
+]);
