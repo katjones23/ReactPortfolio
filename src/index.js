09a5a54c10f8010e765c44c80fb6692f3170221e
Katherine Jones <kejone1414@gmail.com>
2020-07-27 18:05:30 -0400

    added 404 file and updated index.html for github

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
