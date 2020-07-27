81ed50e921f4e1ed74ee7796400b60eaff8ba855
Katherine Jones <kejone1414@gmail.com>
2020-07-27 16:33:45 -0400

    feat: added more recent projects

--- src/components/PortfolioCard/index.js
+++ src/components/PortfolioCard/index.js
@@ -1,16 +1,27 @@
 import React from "react";
 import PortImage from "../PortImage"
 
-const PortfolioCard = ({ projectname, description, gif, sitelink, repolink }) => {
-    return (
-        <div className="col-md-6 col-sm-12 card text-center bg-light">
+const PortfolioCard = ({ projectname, description, image, sitelink, repolink }) => {
+    let returnCode = ""
+
+    if (sitelink === repolink) {
+        returnCode = <div className="col-md-6 col-sm-12 card text-center bg-light">
+        <h5 className="card-header">{projectname}</h5>
+        <PortImage source={require("../../images/" + image)} altTxt={projectname} />
+        <p className="card-body portText">{description}</p>
+        <a className="repoLink" href={repolink} title="RepoLink" rel="noopener noreferrer" target="_blank">Repo Link</a>
+    </div>
+    } else {
+        returnCode = <div className="col-md-6 col-sm-12 card text-center bg-light">
             <h5 className="card-header">{projectname}</h5>
-            <PortImage source={require("../../images/" + gif)} altTxt={projectname} />
+            <PortImage source={require("../../images/" + image)} altTxt={projectname} />
             <p className="card-body portText">{description}</p>
             <a href={sitelink} title="SiteLink" rel="noopener noreferrer" target="_blank">Site Link</a>
             <a className="repoLink" href={repolink} title="RepoLink" rel="noopener noreferrer" target="_blank">Repo Link</a>
         </div>
-    );
+    }
+
+    return (returnCode);
 };
 
 export default PortfolioCard;
